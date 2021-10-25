4ba94419d3572fce39f5cf0b99c4ed93dc71c9b2
LuisaPinillos <luisapinillos20@gmail.com>
2021-10-25 11:26:22 -0500

    update

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,36 +1,31 @@
-#include "sort.h"
+include "sort.h"
 
 /**
- * bubble_sort - function that sorts in ascending order
- * @array: the array to be sorted
- * @size: the size of the array
+ * insertion_sort_list - function that sorts in ascending order
+ * @list: the array to be sorted
  * Return: this function doesn't return
  */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *head = *list;
-	int ssize = 0;
-	int i = 0, j = 0, key = 0;
+	listint_t *curr;
 
-	head = malloc(sizeof(listint_t));
-
-	if (head == NULL)
-	{
+	if (!list)
 		return;
-	}
-
-	listint_t *temp = head;
-
-	while ((*list) != NULL)
+	if ((*list)->next == NULL)
+		return;
+	while (list)
 	{
-		ssize++;
-		(*list) = (*list)->next;
-	}
-	for(i = 1; i < ssize; i++)
-	{
-		temp = head->next;
-		j = i - i;
+		curr = *list;
+		if (curr->prev != NULL && curr->n < curr->prev->n)
+		{
+			curr->prev->next = curr->next;
+			curr->next = curr->next->prev;
+		}
+		while(curr->prev != NULL)
+		if (curr->prev < curr)
+		{
 
+		}
 	}
 }
