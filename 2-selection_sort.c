ca90110363ab5405708646160f6ea24fbd6b71bd
LuisaPinillos <luisapinillos20@gmail.com>
2021-10-25 19:16:53 -0500

    Function selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,13 @@
+include "sort.h"
+
+/**
+ * selection_sort - function that do selection sort
+ * @array: the array to be sorted
+ * @size: size of the array
+ * Return: this function doesn't return
+ */
+
+ void selection_sort(int *array, size_t size)
+ {
+
+ }
