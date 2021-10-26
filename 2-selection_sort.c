0f9ee2e9589c63d762ec46f49ff2084104e94b2c
LuisaPinillos <luisapinillos20@gmail.com>
2021-10-26 00:35:01 -0500

    function selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,4 +1,4 @@
-include "sort.h"
+#include "sort.h"
 
 /**
  * selection_sort - function that do selection sort
@@ -7,7 +7,32 @@ include "sort.h"
  * Return: this function doesn't return
  */
 
- void selection_sort(int *array, size_t size)
- {
+void selection_sort(int *array, size_t size)
+{
+    size_t i = 0;
+    size_t j = 0;
+    int *list = array;
+    int *listord[size];
 
- }
+    if (!list)
+    {
+        return;
+    }
+    for (i = 0; i < size - 1; i++)
+    {
+        size_t numless = i;
+
+        for (j = i + 1; j < size; j++)
+        {
+            if (list[j] < list[numless])
+            {
+                numless = j;
+            }
+        }
+        if (numless != i)
+        {
+            listord = _swap(*list, i, j);
+            print_list(*listord);
+        }
+    }
+}
