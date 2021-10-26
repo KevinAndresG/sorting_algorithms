109e17930823068bf14342e6a65e40e450a1c516
LuisaPinillos <luisapinillos20@gmail.com>
2021-10-26 12:48:38 -0500

    fix the function selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -35,3 +35,21 @@ void selection_sort(int *array, size_t size)
 		}
 	}
 }
+
+/**
+ * _swap - swap two values from an array
+ * @list: list to do a swap
+ * @valone: first value to swap
+ * @valtwo: second value to swap
+ * Return: a list
+ */
+
+int *_swap(int *list, int valone, int valtwo)
+{
+	int tmp = list[valone];
+
+	list[valone] = list[valtwo];
+	list[valtwo] = tmp;
+
+	return (list);
+}
