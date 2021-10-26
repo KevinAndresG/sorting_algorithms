20a35ea641f9cabf10dff35bde69dc7568c493f0
KevinAndresG <3348@holbertonschool.com>
2021-10-25 19:11:00 -0500

    creating swap function

---
+++ swap.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _swap - swap two values from an array
+ * @value1: first value to swap
+ * @value2: second value to swap
+ * @list: list to do a swap
+ * Return: a list
+ */
+
+int _swap(int list, int value1, int value2)
+{
+	int tmp = list[value1];
+
+	list[value1] = list[value2];
+	list[value2] = tmp;
+
+	return (list);
+}
